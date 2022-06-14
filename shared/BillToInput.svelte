2cd6755a0c5c805516ac5b2319a8c2a7b8add03d
Josh Duff <me@JoshDuff.com>
2022-06-13 22:38:28 -0500

    Support multi-line addresses + more aggressive input type detection

--- shared/BillToInput.svelte
+++ shared/BillToInput.svelte
@@ -17,7 +17,7 @@
 	export let allowed_types: InputTypes[] = [ `email`, `phone`, `address` ]
 	export let current_type: InputTypes | null = null
 	export let value = ``
-	export let element: HTMLInputElement | null = null
+	export let element: HTMLTextAreaElement | null = null
 
 	let allowed_types_set = new Set(allowed_types)
 
@@ -38,13 +38,24 @@
 	const phone_regex = /[\d\s\-.()+]{7,}/
 	const is_phone = (value: string) => phone_regex.test(value)
 
-	const assign_current_type = () => {
-		if (is_email(value)) {
+	const is_definitely_address = (value: string) => /\n/.test(value)
+
+	const is_not_email = (value: string) => /\s/.test(value)
+	const is_not_phone = (value: string) => /[^\d \*\(\)\-_\+\.]/.test(value)
+	const assign_best_guess_at_type = (value: string) => {
+		const can_be_email = allowed_types_set.has(`email`) && !is_not_email(value)
+		const can_be_phone = allowed_types_set.has(`phone`) && !is_not_phone(value)
+
+		const address_by_process_of_elimination = !can_be_email && !can_be_phone
+
+		if (is_definitely_address(value)
+			|| (value.length > 0 && address_by_process_of_elimination)
+		) {
+			current_type = `address`
+		} else if (is_email(value)) {
 			current_type = `email`
 		} else if (is_phone(value)) {
 			current_type = `phone`
-		} else if (value.length > 0) {
-			current_type = `address`
 		} else {
 			current_type = null
 		}
@@ -58,21 +69,58 @@
 		}
 	}
 
-	$: allowed_types_set && assign_current_type()
+	$: replicated_value_for_sizing = allowed_types_set && value + (current_type === `address` ? `\n` : ``)
+
+	const get_replicated_value_for_sizing = (value: string, current_type: string | null) => value + (current_type === `address` ? `\n` : ``)
+
+	$: allowed_types_set && assign_best_guess_at_type(value)
 </script>
 
 <Label>
 	{label}
 	<InputStyle>
-		<input
-			type="text"
-			bind:value
-			on:blur={assign_current_type}
-			bind:this={element}
+		<div
+			class="grow-wrap"
+			data-replicated-value={get_replicated_value_for_sizing(value, current_type)}
 		>
+			<textarea
+				rows=1
+				class=input-style
+				bind:value
+				bind:this={element}
+			/>
+		</div>
 	</InputStyle>
 </Label>
 
 <style>
+	/* cribbed from https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/ */
+	.grow-wrap {
+		display: grid;
+		width: 100%;
+	}
+	.grow-wrap::after {
+		/* Note the weird space! Needed to preventy jumpy behavior */
+		content: attr(data-replicated-value) " ";
 
+		/* This is how textarea text behaves */
+		white-space: pre-wrap;
+
+		/* Hidden from view, clicks, and screen readers */
+		visibility: hidden;
+	}
+	textarea {
+		resize: none;
+		overflow: hidden;
+	}
+	textarea,
+	.grow-wrap::after {
+		/* Identical styling required!! */
+		padding: 4px 8px;
+
+		font: inherit;
+
+		/* Place on top of each other */
+		grid-area: 1 / 1 / 2 / 2;
+	}
 </style>
